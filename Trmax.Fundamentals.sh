c974479fc5f289071d8e19860892ace78ff69bdf
zeroabdullah <47537346+zeroabdullah@users.noreply.github.com>
2020-03-04 15:19:52 +0300

    Trmax.Fundamentals
    Trmax.Fundamentals

---
+++ Trmax.Fundamentals.sh
@@ -0,0 +1,76 @@
+echo "  +=======================================+"
+echo "  |......Repair metasploit-framework......|"
+echo "  +---------------------------------------+"
+echo "  |                                       |"
+echo "  |      www.instagram.com/zero_1zz6      |"
+echo "  |                                       |"
+echo "  |                                       |"
+echo "  |                                       |"
+echo "  |                                       |"
+echo "  +=======================================+"
+echo "  |......Repair metasploit-framework......|"
+echo "  +---------------------------------------+"
+echo "      Gary proves metasploit-framework"
+echo "                I love Zahraa"
+echo "Press Enter if you Agree / Continue Install"
+echo "Press CTRL + C if you Disagree / Cancel Install"
+read ENTER
+
+pkg install update -y
+echo "ok update"
+pkg install upgrad -y
+echo "ok upgrad"
+pkg install python -y
+echo "ok python"
+pkg install python2 -y
+echo "ok python2"
+pkg install git -y
+echo "ok git"
+pkg install ruby -y
+echo "ok ruby"
+pkg install nmap -y
+echo "ok nmap"
+pkg install unzip -y
+echo "unzip"
+pkg install nano -y
+echo "ok nano"
+pkg install php -y
+echo "ok php"
+pkg install php2 -y
+echo "ok php2"
+pkg install golang -y
+echo "ok golang"
+pkg install havij -y
+echo "ok havij"
+pkg install host -y
+echo "ok host"
+pkg install wireshark -y
+echo "ok wireshark"
+pkg install cmatrix -y
+echo "ok cmatrixr"
+pkg install wget -y
+echo "ok wget"
+pkg install curl -y
+echo "ok curl"
+pkg install wgetrc -y
+echo "ok wgetrc"
+pkg install openssh -y
+echo "ok openssht"
+pkg install tor -y
+echo "ok tor"
+pkg install net-tools -y
+echo "ok net-tools"
+pkg install unrar -y
+echo "ok unrar"
+pkg install w3m -y
+echo "ok w3m"
+pkg install proot -y
+echo "ok proot"
+pip2 install requests 
+echo "ok requests"
+gem install bundler
+echo "ok bundler"
+gem install bundle 
+echo "ok bundle"
+pip2 install bundle
+echo "ok ~_~"
